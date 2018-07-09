6983633642471fc8c758efc6a2ecb13186afc366
dkrut <dd.krutikov@gmail.com>
2018-07-09 23:57:38 +0300

    created page objects classes

---
+++ src/test/java/org/bitbucket/dkrut/pages/InboxMessages.java
@@ -0,0 +1,33 @@
+package org.bitbucket.dkrut.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by @author Denis Krutikov on 09.07.2018.
+ */
+
+public class InboxMessages {
+    private WebDriver driver;
+
+    public InboxMessages(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    private By createMessage = By.xpath("//a[@title='Написать письмо (N)']");
+    private By to = By.cssSelector(".compose-head__row-wrapper_to [tabindex=\"4\"]");
+    private By subject = By.cssSelector(".compose-head__row_nohr .compose-head__cell:nth-of-type(2) .b-input");
+    private By frame = By.xpath("//iframe");
+    private By body = By.cssSelector("#tinymce");
+
+    public void createNewMessage(String to, String subject, String body){
+        driver.findElement(createMessage).click();
+        driver.findElement(this.to).sendKeys(to);
+        driver.findElement(this.subject).sendKeys(subject);
+        driver.switchTo().frame(driver.findElement(frame));
+        driver.findElement(this.body).click();
+        driver.findElement(this.body).sendKeys(body);
+        driver.findElement(this.body).sendKeys(Keys.CONTROL, Keys.ENTER);
+    }
+}
